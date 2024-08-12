53e9dd185fd8c33b3c550d7fef3767c0a0994dda
taiimrr <taimoornasir590@gmail.com>
2024-08-12 15:49:36 +0500

    new var

--- var.tf
+++ var.tf
@@ -1,2 +1,14 @@
-this is first variable file
-ok i got it
+variable "REGION" {
+  default = "us-east-2"
+}
+
+variable "ZONE" {
+  default = "us-east-2a"
+}
+
+variable "AMIS" {
+  type = map(any)
+  default = {
+    us-east-2 = "ami-0862be96e41dcbf74"
+  }
+}
